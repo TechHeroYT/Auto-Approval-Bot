30b588cda4a9b6d5b76adda1f2c1f19a2d03aa71
Tech HEro <163621970+TechHeroYT@users.noreply.github.com>
2024-04-18 15:58:33 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,8 +9,8 @@ class Config:
     API_ID = int(getenv("API_ID", "22672907"))
     API_HASH = getenv("API_HASH", "0ff15ae2153bd8e03b48cb293010bc6a")
     BOT_TOKEN = getenv("BOT_TOKEN", "6615592453:AAEgKfReYuZns7Q-FQVFPITAqWT0q2l6_wU")
-    FSUB = getenv("FSUB", "aapna_Movies")
-    CHID = int(getenv("CHID", "-1002056115205"))
+    FSUB = getenv("FSUB", "bots_repo")
+    CHID = int(getenv("CHID", "-1002064960434"))
     SUDO = list(map(int, getenv("SUDO", "6287591671").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://rani828719:sVyRWZOrUzIWNfHp@cluster0.zodktob.mongodb.net/?retryWrites=true&w=majority")
     
